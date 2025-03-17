194b75682e3f18fedf323cef1ee803c4edb362d8
itsLeonB <81101232+itsLeonB@users.noreply.github.com>
2025-03-17 14:34:29 +0000

    fix code style

--- app/Utils/DatetimeUtils.php
+++ app/Utils/DatetimeUtils.php
@@ -13,6 +13,7 @@ class DatetimeUtils
             return Carbon::parse($datetime)->timezone(config('app.timezone'))->format(config('app.datetime.format'));
         } catch (\Exception $e) {
             Log::error($e->getMessage());
+
             return $datetime;
         }
     }
